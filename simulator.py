af47d82199c8f3b77a41f2ebed2358e4b9971f9d
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-05-03 11:01:33 +0100

    Add files via upload

--- simulator.py
+++ simulator.py
@@ -1,6 +1,19 @@
+
+def ranges(n):
+    t=True
+    rets=[]
+    nn=0
+    while t:
+        rets=rets+[nn]
+        nn=nn+1
+        if nn==n or nn>n:
+            t=False
+    return rets
 print ("\033c\033[43;30m\nenter simulator\n")
 x=0
 y=0
-for y in range(10):
-    for x in range(10):
-        print (str(y)+","+str(x))
+z=0
+for z in ranges(3):
+    for y in ranges(3):
+        for x in ranges(3):
+             print (str(z)+","+str(y)+","+str(x))
